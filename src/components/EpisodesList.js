a2399fc9df1921ab29836a3d52484246ae8f354c
Christopher Hernandez <cerberean661@gmail.com>
2019-08-23 10:17:21 -0700

    filling out EpisodesList.js

---
+++ src/components/EpisodesList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import EpisodesCard from './EpisodesCard';
+
+export default function EpisodesList() {
+
+    const [episodes, setEpisodes] = useState();
+
+    useEffect(() => {
+
+    axios
+        .get('https://rickandmortyapi.com/api/episode/')
+        .then(response => {
+            
+          setEpisodes(response.data.results);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }, []);
+
+    if(!episodes) {return null;}
+
+    return (
+        <section className="episodes-list grid-view">
+        {episodes.map(result => (
+            <EpisodesCard key={result.id} episodes={result} />
+        ))}
+        </section>
+    );
+}
